f104e124914fb850b5d139acc73d78c9930c9d15
Chris Kerpa <chris.kerpa@mennen.online>
2022-01-10 10:55:20 +0100

    Token

--- Laralex/src/Laralex.php
+++ Laralex/src/Laralex.php
@@ -1,6 +1,35 @@
 <?php
 
+namespace MennenOnline\Laralex;
+
+use GuzzleHttp;
+
 class Laralex
 {
+    private const apiURL = "https://api.lexoffice.io/";
+    private const token = "§§§§§§§§§§§";
+
+    public static function get(String $endpoint, String $payload) {
+        $client = new GuzzleHttp\Client();
+
+        $request = $client->request('GET', self::apiURL . $endpoint . $payload, [
+            'headers' => [
+                "Authorization" => "Bearer " . self::token,
+                "Accept" => "application/json"
+            ]
+        ]);
+
+        $response = $request->getBody();
+
+        file_put_contents("output.json", $response);
+    }
+
+    public static function getContact(String $id) {
+        Laralex::get("v1/contacts/", $id);
+    }
+
+    public static function getAllContacts() {
+        Laralex::get("v1/contacts/", "?page=0");
+    }
 
 }
--- Laralex/tests/Feature/LaralexTest.php
+++ Laralex/tests/Feature/LaralexTest.php
@@ -2,30 +2,50 @@
 
 namespace MennenOnline\Laralex\Tests;
 
+use Illuminate\Support\Facades\Config;
+use MennenOnline\Laralex\Laralex;
 use Orchestra\Testbench\TestCase;
 use GuzzleHttp;
 
 class LaralexTest extends TestCase
 {
-    public function test_init() {
+    public function test_init()
+    {
         $this->assertTrue(true);
     }
 
-    public function test_retrieve_contact() {
-        $client = new GuzzleHttp\Client();
-        $apiURL = "https://api.lexoffice.io/v1/contacts/";
-        $id = "";
-        $token = "";
+    public function test_functions()
+    {
+        Laralex::getAllContacts();
+    }
 
-        $request = $client->request("GET", $apiURL, [
-           'headers' => [
-               "Authorization: Bearer $token",
-               "Accept: application/json"
-           ]
+/*    public function test_create()
+    {
+        $client = new GuzzleHttp\Client();
+        $apiURL = "https://api.lexoffice.io/v1/contacts";
+        $token = "mTGx6xlO1onKscoDBpgy1h98GK8";
+        $request = $client->request('POST', $apiURL, [
+            'headers' => [
+                "Authorization" => "Bearer " . $token,
+                "Accept" => "application/json",
+                "Content-Type" => "application/json"
+            ],
+            'body' => json_encode([
+                "version" => 0,
+                "roles" => [
+                    "customer" => []
+                ],
+                "person" => [
+                    "salutation" => "Herr",
+                    "firstName" => "Max",
+                    "lastName" => "Mustermann"
+                ],
+                "note" => "Notiz"
+            ], JSON_FORCE_OBJECT)
         ]);
 
         $response = $request->getBody();
 
         dd($response);
-    }
+    }*/
 }
