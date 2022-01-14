059755e63edec8ef4eb557caaebf44dbab131e42
Chris Kerpa <chris.kerpa@mennen.online>
2022-01-14 13:13:07 +0100

    New Features

--- Laralex/src/Laralex.php
+++ Laralex/src/Laralex.php
@@ -4,12 +4,24 @@ namespace MennenOnline\Laralex;
 
 use GuzzleHttp;
 
+/**
+ * Class LaraLex
+ * The LaraLex Class contains all main functions in order to retrieve data from the LexOffice API
+ */
 class Laralex
 {
+    private const DEBUG = true;
     private const apiURL = "https://api.lexoffice.io/";
-    private const token = "§§§§§§§§§§§";
+//    private const token = ""; //REAL
+    private const token = "mTGx6xlO1onKscoDBpgy1h98GK8"; //TEST
 
-    public static function get(String $endpoint, String $payload) {
+    public static function getContact(string $id): array
+    {
+        return Laralex::get("v1/contacts/", $id);
+    }
+
+    public static function get(string $endpoint, string $payload): array
+    {
         $client = new GuzzleHttp\Client();
 
         $request = $client->request('GET', self::apiURL . $endpoint . $payload, [
@@ -21,15 +33,54 @@ class Laralex
 
         $response = $request->getBody();
 
-        file_put_contents("output.json", $response);
+        // Enable logging if DEBUG is true
+        if (self::DEBUG) {
+            file_put_contents("output.php", print_r(json_decode($response, true), true));
+        }
+
+        return json_decode($response, true);
     }
 
-    public static function getContact(String $id) {
-        Laralex::get("v1/contacts/", $id);
+    public static function getAllContacts(): array
+    {
+        return Laralex::get("v1/contacts/", "?page=0");
     }
 
-    public static function getAllContacts() {
-        Laralex::get("v1/contacts/", "?page=0");
+    public static function createPerson(array $role, array $person, string $note)
+    {
+        $merge = [
+            'version' => 0,
+            'roles' => $role,
+            'person' => $person,
+            'note' => $note
+        ];
+
+        if (self::DEBUG) {
+            Laralex::log(json_encode($merge), "json");
+        }
+
+        Laralex::post("v1/contacts/", $merge);
     }
+
+    private static function log($input, string $type)
+    {
+        file_put_contents("output." . $type, print_r($input, true));
+    }
+
+    private static function post($endpoint, $payload)
+    {
+        $client = new GuzzleHttp\Client();
+
+        $request = $client->request("POST", self::apiURL . $endpoint, [
+            'headers' => [
+                "Authorization" => "Bearer " . self::token,
+                "Accept" => "application/json",
+                "Content-Type" => "application/json"
+            ],
+            "body" => json_encode($payload, JSON_FORCE_OBJECT)
+        ]);
+
+    }
+
 
 }
--- Laralex/tests/Feature/LaralexTest.php
+++ Laralex/tests/Feature/LaralexTest.php
@@ -16,36 +16,39 @@ class LaralexTest extends TestCase
 
     public function test_functions()
     {
-        Laralex::getAllContacts();
+        $array = Laralex::getAllContacts();
+//        $array = LaraLex::getContact("5f0bfb5a-f243-446f-87ba-ac5590bd9834");
+//        Laralex::createPerson(["customer" => []], ["salutation" => "Herr", "firstName" => "Testimus", "lastName" => "Prime"], "");
+        dd($array);
     }
 
-/*    public function test_create()
-    {
-        $client = new GuzzleHttp\Client();
-        $apiURL = "https://api.lexoffice.io/v1/contacts";
-        $token = "mTGx6xlO1onKscoDBpgy1h98GK8";
-        $request = $client->request('POST', $apiURL, [
-            'headers' => [
-                "Authorization" => "Bearer " . $token,
-                "Accept" => "application/json",
-                "Content-Type" => "application/json"
-            ],
-            'body' => json_encode([
-                "version" => 0,
-                "roles" => [
-                    "customer" => []
+    /*    public function test_create()
+        {
+            $client = new GuzzleHttp\Client();
+            $apiURL = "https://api.lexoffice.io/v1/contacts";
+            $token = "mTGx6xlO1onKscoDBpgy1h98GK8";
+            $request = $client->request('POST', $apiURL, [
+                'headers' => [
+                    "Authorization" => "Bearer " . $token,
+                    "Accept" => "application/json",
+                    "Content-Type" => "application/json"
                 ],
-                "person" => [
-                    "salutation" => "Herr",
-                    "firstName" => "Max",
-                    "lastName" => "Mustermann"
-                ],
-                "note" => "Notiz"
-            ], JSON_FORCE_OBJECT)
-        ]);
+                'body' => json_encode([
+                    "version" => 0,
+                    "roles" => [
+                        "customer" => []
+                    ],
+                    "person" => [
+                        "salutation" => "Herr",
+                        "firstName" => "Max",
+                        "lastName" => "Mustermann"
+                    ],
+                    "note" => "Notiz"
+                ], JSON_FORCE_OBJECT)
+            ]);
 
-        $response = $request->getBody();
+            $response = $request->getBody();
 
-        dd($response);
-    }*/
+            dd($response);
+        }*/
 }
